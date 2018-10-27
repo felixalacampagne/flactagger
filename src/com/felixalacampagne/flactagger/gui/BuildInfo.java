936e95828ee43a27a78a8aa6627c0f0f4887722d
Chris <chris@felixalacampagne.be>
2018-10-27 13:24:27 +0200

    Try to stop excessive generating of BuildInfo

--- src/com/felixalacampagne/flactagger/gui/BuildInfo.java
+++ src/com/felixalacampagne/flactagger/gui/BuildInfo.java
@@ -3,6 +3,6 @@ package com.felixalacampagne.flactagger.gui;
 
 public class BuildInfo
 {
-public static final String VERSION = "18.10.27.1314";
+public static final String VERSION = "18.10.27.1322";
 private BuildInfo() {}
 }
