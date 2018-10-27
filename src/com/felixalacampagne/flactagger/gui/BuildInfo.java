230d2c3405e131933467e01efef12a6f96e7143c
Chris <chris@felixalacampagne.be>
2018-10-27 13:14:19 +0200

    27-Oct-2018 This is a generated file but including in git for my convenience

--- src/com/felixalacampagne/flactagger/gui/BuildInfo.java
+++ src/com/felixalacampagne/flactagger/gui/BuildInfo.java
@@ -3,6 +3,6 @@ package com.felixalacampagne.flactagger.gui;
 
 public class BuildInfo
 {
-public static final String VERSION = "18.10.27.1310";
+public static final String VERSION = "18.10.27.1313";
 private BuildInfo() {}
 }
