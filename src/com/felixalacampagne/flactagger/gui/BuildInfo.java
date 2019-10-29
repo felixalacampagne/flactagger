51e6e05ff7a0a2964c665a756e363102467d7ad7
Chris <chris@felixalacampagne.be>
2019-10-29 22:56:29 +0100

    NAS git is still being fucked up by Git Desktop

--- src/com/felixalacampagne/flactagger/gui/BuildInfo.java
+++ src/com/felixalacampagne/flactagger/gui/BuildInfo.java
@@ -3,6 +3,6 @@ package com.felixalacampagne.flactagger.gui;
 
 public class BuildInfo
 {
-public static final String VERSION = "19.10.29.2247";
+public static final String VERSION = "19.10.29.2249";
 private BuildInfo() {}
 }
