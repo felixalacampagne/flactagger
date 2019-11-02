49317adf745123aa0835272d22365258f07cdb4a
Chris <chris@felixalacampagne.be>
2019-11-02 18:34:16 +0100

    Remove DelthasMP3 references

--- src/com/felixalacampagne/flactagger/gui/BuildInfo.java
+++ src/com/felixalacampagne/flactagger/gui/BuildInfo.java
@@ -3,6 +3,6 @@ package com.felixalacampagne.flactagger.gui;
 
 public class BuildInfo
 {
-public static final String VERSION = "19.11.02.1809";
+public static final String VERSION = "19.11.02.1833";
 private BuildInfo() {}
 }
