cb6ff1d198213c7431cace2447713b15755c8888
Chris <chris@felixalacampagne.be>
2020-01-05 13:48:48 +0100

    New build

--- src/com/felixalacampagne/flactagger/gui/BuildInfo.java
+++ src/com/felixalacampagne/flactagger/gui/BuildInfo.java
@@ -3,6 +3,6 @@ package com.felixalacampagne.flactagger.gui;
 
 public class BuildInfo
 {
-public static final String VERSION = "20.01.03.1932";
+public static final String VERSION = "20.01.04.1159";
 private BuildInfo() {}
 }
