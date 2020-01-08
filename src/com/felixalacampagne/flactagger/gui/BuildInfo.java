8c2a3f13bb3a0c06f3fbc3a54a295b136fa2c2c4
Chris <chris@felixalacampagne.be>
2020-01-08 08:25:25 +0100

    Stop creating BuildInfo everytime I click something! ie. disable autobuild which is constantly invoked by eclipse for no reason, eg. after a git commit!

--- src/com/felixalacampagne/flactagger/gui/BuildInfo.java
+++ src/com/felixalacampagne/flactagger/gui/BuildInfo.java
@@ -3,7 +3,7 @@ package com.felixalacampagne.flactagger.gui;
 
 public class BuildInfo
 {
-private static String VERSION = "20.01.08.0819";
+private static String VERSION = "20.01.08.0823";
 	public BuildInfo() {}
 	public String getVersion()
 	{
