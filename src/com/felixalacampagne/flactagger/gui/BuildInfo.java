858133585af4d0e5e3097c75b4013d219f84a872
Chris <chris@felixalacampagne.be>
2020-01-08 08:27:21 +0100

    Ooops. Forgot to remove the depends from the dummyautobuild target.

--- src/com/felixalacampagne/flactagger/gui/BuildInfo.java
+++ src/com/felixalacampagne/flactagger/gui/BuildInfo.java
@@ -3,7 +3,7 @@ package com.felixalacampagne.flactagger.gui;
 
 public class BuildInfo
 {
-private static String VERSION = "20.01.08.0825";
+private static String VERSION = "20.01.08.0826";
 	public BuildInfo() {}
 	public String getVersion()
 	{
