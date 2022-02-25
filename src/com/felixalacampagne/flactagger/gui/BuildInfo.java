3746715cc8fb192404abca8d4be8769a33217ab4
chris <chris@chunky>
2022-02-25 10:51:12 +0100

    Update buildinfo

--- src/com/felixalacampagne/flactagger/gui/BuildInfo.java
+++ src/com/felixalacampagne/flactagger/gui/BuildInfo.java
@@ -3,7 +3,7 @@ package com.felixalacampagne.flactagger.gui;
 
 public class BuildInfo
 {
-private static String VERSION = "22.02.23.1429";
+private static String VERSION = "22.02.25.0859";
 	public BuildInfo() {}
 	public String getVersion()
 	{
